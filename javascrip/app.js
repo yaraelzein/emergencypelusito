698167ed6144702f32af046205d156d0cdc697a7
yaraelzein <yaraelzein2001@gmail.com>
2021-10-18 12:34:52 -0500

    new project panda

---
+++ javascrip/app.js
@@ -0,0 +1,12 @@
+let buttonElement = document.getElementsByClassName("button")[0];
+let gifElement = document.getElementsByClassName("image")[0];
+let audioElement = document.getElementsByClassName("music")[0];
+
+buttonElement.addEventListener("click", () => {
+    buttonElement.parentNode.removeChild(buttonElement)
+    audioElement.play()
+    gifElement.style.visibility = "visible"
+})
+
+
+
